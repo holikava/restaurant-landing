89bbbbd117be7d2c5c55c659823ad90559959a94
Tanya Holikava <holikavadev@gmail.com>
2023-09-21 20:12:44 +0300

    rewrite slider

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // **** Navigation *****
 
 const navToggle = document.querySelector('.hamburger-menu-toggle');
@@ -26,4 +28,43 @@ for (let anchor of anchors) {
       block: 'start'
     })
   })
-}
+};
+
+// slider 
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const slides = document.querySelectorAll('.testimonial__card');
+
+slides.forEach(function(slide, index) {
+  slide.style.left = `${index * 100}%`;
+  console.log(slide);
+});
+
+let counter = 0;
+
+prevBtn.addEventListener('click', () => {
+  counter--;
+  carousel();
+});
+
+nextBtn.addEventListener('click', () => {
+  counter++;
+  carousel();
+});
+
+function carousel() {
+  if(counter < slides.length - 1) {
+    nextBtn.style.display = 'inline-block';
+  } else {
+    nextBtn.style.display = 'none';
+  }
+  if(counter > 0) {
+    prevBtn.style.display = 'inline-block';
+  } else {
+    prevBtn.style.display = 'none';
+  }
+  slides.forEach( (slide) => {
+    slide.style.transform = `translateX(-${counter * 100}%)`;
+  });
+};
